e333b20f58fc75cd508b0a70bef8d09d3f82f371
TienHao <haokn97@mail.com>
2020-03-27 22:10:44 +0700

    new app

---
+++ MusicOnlineApp/App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import { SideMenu } from './src/Components/Router';
+
+export default class App extends Component {
+    render() {
+        return (
+            <SideMenu />
+        );
+    }
+}
